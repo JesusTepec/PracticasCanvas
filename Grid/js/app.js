a7963adc1a5c366fc2674ac25ffb2d7a99e73478
Jesús Tepec <ttpecjn@gmail.com>
2018-02-07 00:43:46 -0600

    Mantener cuadricula - Grid
    -Mantener visible cuadricula al dibujar
    -Fix. codigo
    -Tamaño de cuadros

--- Grid/js/app.js
+++ Grid/js/app.js
@@ -1,90 +1,111 @@
-var color = "red";
-
-function cambiarColor() {
-  color = document.getElementById("color").value;
-}
 
 window.onload = function() {
-  var mouse = false;
-  var canvas = document.getElementById("canvas1");
-  var contenedor = document.getElementById("Contenedor");
-  var cuadritos = [];
+    var mouse = false;
+    var canvas = document.getElementById("canvas1");
+    var contenedor = document.getElementById("Contenedor");
+    var cuadritos = [];
+    var sizeCuadro = {ancho: 25, alto: 25};
+    var color = "red";
+    var inputColor = document.getElementById("color");
+  //  var chkGrid = document.getElementById("chkCuadricula");
+    var inputSizeCuadros = document.getElementById("sizeCuadros");
+    var dibujarGrid = true;
 
-  if (canvas && canvas.getContext) {
-    var ctx = canvas.getContext("2d");
-    if (ctx) {
-      canvas.width = contenedor.offsetWidth - 400;
+    if (canvas && canvas.getContext) {
+        var ctx = canvas.getContext("2d");
+        if (ctx) {
+            canvas.width = contenedor.offsetWidth - 400;
 
+            function limparArea(x, y, ancho, alto) {
+                ctx.clearRect(x, y, ancho, alto);
+            }
 
-      function dibujaGrid(disX, disY, ancho, color) {
-        ctx.strokeStyle = color;
-        ctx.lineWidth = 0.2;
-        var columnas = [];
-        for (var i = disX + ancho; i < canvas.width; i += disX) {
-          ctx.beginPath();
-          ctx.moveTo(i, 0);
-          ctx.lineTo(i, canvas.height);
-          ctx.stroke();
-          columnas.push(i);
+            function dibujaGrid(disX, disY, anchoLinea, color){
+                ctx.strokeStyle = color;
+                ctx.lineWidth = anchoLinea;
+                var columnas = [];
+                var filas = [];
+
+                for (var i = disX; i < canvas.width; i += disX) {
+                    ctx.beginPath();
+                    ctx.moveTo(i, 0);
+                    ctx.lineTo(i, canvas.height);
+                    ctx.stroke();
+                    columnas.push(i);
+                }
+                for (var i = disY; i < canvas.height; i += disY) {
+                    ctx.beginPath();
+                    ctx.moveTo(0, i);
+                    ctx.lineTo(ctx.canvas.width, i);
+                    ctx.stroke();
+                    filas.push(i);
+                }
+                columnas.push(0);
+                filas.push(0);
+                for (x = 0; x < columnas.length; x++) {
+                    for (y = 0; y < filas.length; y++) {
+                        cuadritos.push([columnas[x], columnas[y], disX, disY]);
+                    }
+                }
+            }
+
+            function fillCell(x, y, color) {
+                ctx.fillStyle = color;
+                for (i = 0; i < cuadritos.length; i++) {
+                    var cuadro = cuadritos[i];
+                    if (
+                        x > cuadro[0] &&
+                        x < cuadro[0] + cuadro[2] &&
+                        y > cuadro[1] &&
+                        y < cuadro[1] + cuadro[3]
+                    ) {
+                        ctx.fillRect(cuadro[0], cuadro[1], sizeCuadro.ancho, sizeCuadro.alto);
+                    }
+                }
+                if(dibujarGrid)
+                  dibujaGrid(sizeCuadro.ancho, sizeCuadro.alto, 0.4, "Red");
+            }
+
+            canvas.onmousemove = function(e) {
+                if(mouse){
+                    var canvaspos = canvas.getBoundingClientRect();
+                    fillCell(e.clientX - canvaspos.left, e.clientY - canvaspos.top, color);
+                }
+            };
+
+            canvas.onclick = function(e){
+                var canvaspos = canvas.getBoundingClientRect();
+                fillCell(e.clientX - canvaspos.left, e.clientY - canvaspos.top, color)
+            };
+
+            canvas.onmousedown = function(e) {
+                mouse = true;
+            };
+
+            canvas.onmouseup = function(e) {
+                mouse = false;
+            };
+
+            inputColor.addEventListener('change', function() {
+                color = this.value;
+            }, false);
+/*
+            chkGrid.onclick = function () {
+                dibujarGrid = this.checked;
+            };
+*/
+            inputSizeCuadros.addEventListener('change', function () {
+                sizeCuadro.ancho = parseInt(this.value);
+                sizeCuadro.alto = parseInt(this.value);
+                limparArea(0, 0, canvas.width, canvas.height);
+                cuadritos = [];
+                dibujaGrid(sizeCuadro.ancho, sizeCuadro.alto, 1, "Red");
+            }, false);
+
+            dibujaGrid(sizeCuadro.ancho, sizeCuadro.alto, 1, "Red");
+
+        } else {
+            alert("No se pudo cargar el contexto");
         }
-
-        var filas = [];
-        for (var i = disY + ancho; i < canvas.height; i += disY) {
-          ctx.beginPath();
-          ctx.moveTo(0, i);
-          ctx.lineTo(ctx.canvas.width, i);
-          ctx.stroke();
-          filas.push(i);
-        }
-
-        columnas.push(0);
-        filas.push(0);
-        for (x = 0; x < columnas.length; x++) {
-          for (y = 0; y < columnas.length; y++) {
-            cuadritos.push([columnas[x], columnas[y], 15, 15]);
-          }
-        }
-      }
-
-      function fillCell(x, y, color) {
-        ctx.fillStyle = color;
-        for (i = 0; i < cuadritos.length; i++) {
-          var cuadro = cuadritos[i];
-          if (
-            x > cuadro[0] &&
-            x < cuadro[0] + cuadro[2] &&
-            y > cuadro[1] &&
-            y < cuadro[1] + cuadro[3]
-          ) {
-            ctx.fillRect(cuadro[0], cuadro[1], 15, 15);
-          }
-        }
-      }
-
-      canvas.onmousemove = function(e) {
-        if(mouse){
-          var canvaspos = canvas.getBoundingClientRect();
-          fillCell(e.clientX - canvaspos.left, e.clientY - canvaspos.top, color);
-        }
-      };
-
-      canvas.onclick = function(e){
-        var canvaspos = canvas.getBoundingClientRect();
-        fillCell(e.clientX - canvaspos.left, e.clientY - canvaspos.top, color)
-      }
-
-      canvas.onmousedown = function(e) {
-        mouse = true;
-      };
-
-      canvas.onmouseup = function(e) {
-        mouse = false;
-      };
-
-      dibujaGrid(15, 15, 0, color);
-
-    } else {
-      alert("No se pudo cargar el contexto");
     }
-  }
 };
